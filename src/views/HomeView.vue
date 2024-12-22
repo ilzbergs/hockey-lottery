f8114c1936c854474fd3bc0244df00ba60a6193b
ilzbergs <ilzbergs@inbox.lv>
2024-12-22 22:37:59 +0200

    added test data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
   </main>
+   <ul>
+      <li v-for="(user, index) in users" :key="index">{{ user.name }}</li>
+    </ul>
 </template>
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+ <script lang="ts">
+interface User {
+  name: string;
+}
+
+const users: Ref<User[]> = ref([]);
+ const apiUrl = 'http://localhost:3000/users'; // vai pilns URL uz serveri
+
+fetch(apiUrl)
+  .then(response => response.json())
+   .then(fetchedData => {
+      users.value = fetchedData;  // Atjaunina data ref ar iegūtajiem datiem
+      console.log('Users:', fetchedData);  // Attēlo datus konsolē
+    })
+  .catch(error => {
+    console.error('Error fetching users:', error);
+  });
+
+</script>
